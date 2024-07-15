eb5a2018d4769e17a5d186d1d20ebd7395a6efd1
Victor0706 <152538117+Victor0706@users.noreply.github.com>
2024-07-15 23:24:42 +0300

    Add files via upload

---
+++ accounts/forms.py
@@ -0,0 +1,26 @@
+from allauth.account.forms import SignupForm
+from django.core.mail import send_mail, mail_managers, mail_admins
+
+
+class CustomSignupForm(SignupForm):
+    def save(self, request):
+        user = super().save(request)
+
+        send_mail(
+            subject='Добро пожаловать в наш интернет-магазин!',
+            message=f'{user.username}, вы успешно зарегистрировались!',
+            from_email=None,  # будет использовано значение DEFAULT_FROM_EMAIL
+            recipient_list=[user.email],
+        )
+
+        mail_managers(
+            subject='Новый пользователь!',
+            message=f'Пользователь {user.username} зарегистрировался на сайте.'
+        )
+
+        mail_admins(
+            subject='Новый пользователь!',
+            message=f'Пользователь {user.username} зарегистрировался на сайте.'
+        )
+
+        return user
